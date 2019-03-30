3383597ce8c8087309e779c374b1c9564a45577a
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-03-30 12:20:40 +0300

    ders18 thread sekronizasyon

--- Ders18/src/isparcasiornek/KlavyeMouseUretici.java
+++ Ders18/src/isparcasiornek/KlavyeMouseUretici.java
@@ -16,6 +16,8 @@ public class KlavyeMouseUretici extends Thread {
     @Override
     public void run() {
 
+        //İş parçacığı her seferinde 1500 ms uyku moduna geçiyor daha sonra
+        //kaldığı yerden devam ediyor.while true olduğu için bu işlemi tekrar, tekrar yapıyor.
         while (true) {
             try {
 
@@ -24,6 +26,7 @@ public class KlavyeMouseUretici extends Thread {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            //depodaki ürün miktarını senkron bir şekilde 1 artıroyor.
             depo.put();
         }
 
--- Ders18/src/isparcasiornek/Magaza1.java
+++ Ders18/src/isparcasiornek/Magaza1.java
@@ -20,13 +20,16 @@ public class Magaza1 extends Thread {
     @Override
     public void run() {
 
+        //İş parçacığı her seferinde 1500 ms uyku moduna geçiyor daha sonra
+        //kaldığı yerden devam ediyor.while true olduğu için bu işlemi tekrar, tekrar yapıyor.
         while (true) {
             try {
                 sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            //Thread 3000 ms bekle süresi bitikten sonra
+            //Depo nesnelerin de get mothodu yardımyla miktarı 1 azaltıyor.
             monitorDepo.get();
             klavyeMouseDepo.get();
             kasaDepo.get();
