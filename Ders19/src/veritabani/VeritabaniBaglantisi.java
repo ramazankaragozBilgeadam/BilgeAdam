bec16758a70bd9c9159b36aa49816a21eca2426b
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-03-31 11:38:47 +0300

    ders19 veritabanı baglantısı

---
+++ Ders19/src/veritabani/VeritabaniBaglantisi.java
@@ -0,0 +1,44 @@
+package veritabani;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class VeritabaniBaglantisi {
+
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Driver sınıfı bulundu.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Postgresql driver sınıfı bulunamadı.");
+        }
+
+        Connection conn=null;
+
+
+        try {
+            conn= DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres?currentSchema=public",
+                    "postgres","root");
+            System.out.println("Veritabanı bağlantısı gerçekleşti.");
+        } catch (SQLException e) {
+            System.err.println("Veritabanına bağlanamadı.");
+            e.printStackTrace();
+        }finally {
+            if (conn!=null){
+                try {
+                    conn.close();
+                    System.out.println("Bağlantı kapatıldı.");
+                } catch (SQLException e) {
+                    System.err.println("Bağlantı katılamadı.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+}
