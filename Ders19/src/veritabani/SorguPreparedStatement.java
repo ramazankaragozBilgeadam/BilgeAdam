90cf2959d1dd33fa889c9a51af17b8abd7f7ace9
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-03-31 13:23:40 +0300

    ders19 veritabanı baglantısı

--- Ders19/src/veritabani/SorguPreparedStatement.java
+++ Ders19/src/veritabani/SorguPreparedStatement.java
@@ -1,8 +1,6 @@
 package veritabani;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SorguPreparedStatement {
 
@@ -16,7 +14,23 @@ public class SorguPreparedStatement {
                     ,VeritabaniBilgileriEnum.SIFRE.getValue());
             System.out.println("Bağlantı sağlandı.");
 
+            String sorgu="INSERT INTO public.\"Personel\" (ad,soyad,tc_no,tel,dogum_tarihi) VALUES(?,?,?,?,?)";
 
+            PreparedStatement statement=conn.prepareStatement(sorgu);
+
+            statement.setString(1,"Veli");
+            statement.setString(2,"Güt");
+            statement.setString(3,"12345678912");
+            statement.setString(4,"5312565654");
+            statement.setDate(5, Date.valueOf("1989-05-15"));
+
+            int durum=statement.executeUpdate();
+
+            if (durum==1){
+                System.out.println("Veri ekleme işlemi gerçekleştirildi.");
+            }else {
+                System.err.println("Veri eklenemedi!!");
+            }
 
 
         } catch (SQLException e) {
--- Ders19/src/veritabani/VeritabaniBaglantisi.java
+++ Ders19/src/veritabani/VeritabaniBaglantisi.java
@@ -20,25 +20,6 @@ public class VeritabaniBaglantisi {
                     "postgres","root");
             System.out.println("Veritabanı bağlantısı gerçekleşti.");
 
-            String sorgu="INSERT INTO public.\"Personel\" (ad,soyad,tc_no,tel,dogum_tarihi) VALUES(?,?,?,?,?)";
-
-            PreparedStatement statement=conn.prepareStatement(sorgu);
-
-            statement.setString(1,"Can");
-            statement.setString(2,"Güt");
-            statement.setString(3,"12345678912");
-            statement.setString(4,"5312565654");
-            statement.setDate(5, Date.valueOf("1989-05-15"));
-
-            int durum=statement.executeUpdate();
-
-            if (durum==1){
-                System.out.println("Veri ekleme işlemi gerçekleştirildi.");
-            }else {
-                System.err.println("Veri eklenemedi!!");
-            }
-
-
         } catch (SQLException e) {
             System.err.println("Veritabanına bağlanamadı.");
             e.printStackTrace();
