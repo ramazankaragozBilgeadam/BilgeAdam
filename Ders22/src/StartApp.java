dff77d3afdae88cb731febf18447b75f8bba58d9
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-04-13 10:50:25 +0300

    design patterns singleton ve factory

---
+++ Ders22/src/StartApp.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+
+        JFrame ilkPencere=new JFrame("İlk Penceremiz");
+        //ilkPencere.setTitle("İlk Penceremiz");
+        //Penceremizin boyutunu ayarlıyoruz.
+        ilkPencere.setSize(400,400);
+        //Penceremizin görünürlüğünü ayarlıyoruz.
+        ilkPencere.setVisible(true);
+        //Penceremizin konumunu ayarlamak için
+        //Center(orta) kısımda açılması için null set ediyoruz.
+        ilkPencere.setLocationRelativeTo(null);
+        //Penceremizin kaptılması için default close ayarını yapıyoruz.
+        ilkPencere.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Penceremizdeki componentleri konumlandırmak için panel oluşturuyoruz.
+        JPanel panel=new JPanel();
+        //Componentlerimizin soldan sağa doğru sıralanması için layout oluşturduk.
+        FlowLayout flowLayout=new FlowLayout(FlowLayout.LEFT,40,40);
+        //Layout objemizi set ediyoruz.
+        panel.setLayout(flowLayout);
+
+        //Etiket componentimizi oluşturuyoruz.
+        JLabel jLabel=new JLabel("Adı: ");
+
+        //İnput componentini oluşturuyoruz kullanıcıdan bir değer almak için.
+        JTextField jTextField=new JTextField(20);
+
+
+        //Label componentini ekliyoruz.
+        panel.add(jLabel);
+        panel.add(jTextField);
+        ilkPencere.add(panel);
+
+
+
+
+
+
+    }
+}
